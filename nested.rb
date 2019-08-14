b1bc6afaffd98ac47cf6e7eb55f73f888ea97e39
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-14 14:47:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,7 +2,10 @@
 def hopper
 	programmer_hash = 
  		{
-     
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
