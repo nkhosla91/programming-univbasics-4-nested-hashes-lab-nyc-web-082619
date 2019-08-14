38065f44db8a5fd79cf906210c2a7ad6e19cd3c1
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-14 14:55:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,12 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
+
+
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash[:]
 	
 	programmer_hash = 
  		{
